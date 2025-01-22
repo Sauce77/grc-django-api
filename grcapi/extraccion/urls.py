1aebea567678968e351b5c1230c2a1f2ba070474
Sauce77 <sauceortediego505@gmail.com>
2025-01-22 13:48:04 -0600

    se anadio put y delete

--- grcapi/extraccion/urls.py
+++ grcapi/extraccion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import root,RegistroList
+from .views import root,RegistroList, RegistroDetail
 
 urlpatterns = [
     path('', root),
     path("registros/", RegistroList.as_view()),
+    path("registro/<int:pk>", RegistroDetail.as_view()),
 ]
--- grcapi/extraccion/views.py
+++ grcapi/extraccion/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -14,6 +15,9 @@ def root(request):
     return(HttpResponse("root"))
 
 class RegistroList(APIView):
+    """
+        Enlista los registros de la base de datos.
+    """
     def get(self, request):
         registros = Registro.objects.all() 
         serializer = RegistroSerializer(registros, many=True)
@@ -26,3 +30,33 @@ class RegistroList(APIView):
             return Response(registro.data, status=status.HTTP_201_CREATED)
         
         return Response(registro.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class RegistroDetail(APIView):
+    """
+        Selecciona un regsitro especifico con base en la llave primaria.
+    """
+    def get_registro(self,pk):
+        try:
+            return Registro.objects.get(pk=pk)
+        except Registro.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        registro = self.get_registro(pk)
+        serializer = RegistroSerializer(registro)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        registro = self.get_registro(pk)
+        serializer = RegistroSerializer(registro, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        registro = self.get_registro(pk)
+        registro.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
