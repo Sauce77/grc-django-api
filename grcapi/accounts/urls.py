ef2738ec8e55e866ff93e4e7963cb0010eb9411c
Sauce77 <sauceortediego505@gmail.com>
2025-01-24 13:18:06 -0600

    se agrego GET con autorizacion

--- grcapi/accounts/urls.py
+++ grcapi/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import UserList, registro, login
+from .views import list_users, registro, login
 
 urlpatterns = [
-    path("users/", UserList.as_view()),
+    path("users/", list_users, name="users"),
     path("registro/", registro, name="registro"),
     path("login/", login, name="login")
 ]
--- grcapi/accounts/views.py
+++ grcapi/accounts/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.models import User
 
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
+from rest_framework.decorators import permission_classes,authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 from .serializers import UserSerializer
 # Create your views here.
 
-class UserList(APIView):
+@api_view(["GET"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def list_users(request):
     """
         Enlista todos los usuarios registrados.
     """
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
     
 @api_view(["POST"])
 def login(request):
